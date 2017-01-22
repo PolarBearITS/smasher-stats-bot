a4adb289a2944be7e2815f0a9962c166f334ef4e
PolarBearITS <polar.bear.in.the.snow@gmail.com>
2017-01-21 21:48:45 -0500

    basic bot calling implemented

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
 import praw
+from docopt import docopt
+from smasherstats import *
 
-r = praw.Reddit('smasher-stats-bot', user_agent='Bot for scraping smash stats off the net')
-print(r)
+name = 'smasher-stats-bot'
+
+if True:
+	docstring = ''
+	r = praw.Reddit(name, user_agent='Smasher stats web scraper by /u/PolarBearITS')
+	for post in r.subreddit('smasherstats').hot(limit=10):
+		for comment in post.comments.list():
+			if f'/u/{name}' in comment.body and comment.author != name and all(c.author != name for c in comment.replies):
+				comment.reply('Successful call to bot.')
+				print('Replied to /u/' + str(comment.author))
+				docstring = comment.body
